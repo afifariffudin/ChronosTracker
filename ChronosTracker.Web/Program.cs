0328009d9553bd9653b6e285b1214827121edf00
afifariffudin <afifariffudin@gmail.com>
2026-03-07 14:43:19 +0800

    Update DI setup: add services & tweak DbContext config
    Enhanced dependency injection in Program.cs by registering ExcelImportService and IGDBService as scoped services, and enabling HTTP client support. Also modified AppDbContext registration to include an additional parameter for UseSqlServer.

--- ChronosTracker.Web/Program.cs
+++ ChronosTracker.Web/Program.cs
@@ -5,7 +5,7 @@ using ChronosTracker.Infrastructure.Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
 builder.Services.AddScoped<ExcelImportService>();
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IGDBService>();
